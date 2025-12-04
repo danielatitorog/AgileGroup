1bdfcebd253bda4325cf49b1016e2bde8f0b27e7
afmiendez <afonso.m.f.s.mendes@gmail.com>
2025-12-04 19:20:18 +0000

    just testing

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 // Include the required model classes
 require_once('Models/UserAuthentication.php');
 
+//commit test
 // Create a stdClass object to hold data for the view
 $view = new stdClass();
 $view->user = new User();               // Instantiate the User object for authentication
