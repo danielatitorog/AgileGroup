37c92d1f3b2181af58c47c8928537854f6bbc125
Daniela Titorog <daniela.titorog@gmail.com>
2025-12-12 12:55:25 +0000

    Revert "Reapply "Small fix""
    This reverts commit 50a3bd6059c4d255b31292be6d9c2ea6c4ca3269.

---
+++ roadmap.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('Models/UserAuthentication.php');
+require_once('Models/ProfileModel.php');
+
+$view = new stdClass();
+$view->user = new User();
+
+if (!$view->user->isLoggedIn()) {
+    header("Location: index.php");
+    exit;
+}
+
+$view->profileModel = new ProfileModel();
+$view->modules = $view->profileModel->getModules();
+
+// NEW: load last visited slide
+$view->lastPage = $view->profileModel->getLastVisitedPage($_SESSION['user_id']);
+
+require_once('Views/roadmap.phtml');
